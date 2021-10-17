00caf1f9cddd352d7dce51965261af784300f5df
Bart Slinger <bartslinger@gmail.com>
2021-10-17 18:30:56 +0200

    parse procedures into procedure types

--- src/krpc_gen/lib.rs
+++ src/krpc_gen/lib.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io::BufReader;
 use std::collections::HashMap;
 use serde::Deserialize;
+use regex::Regex;
 
 #[derive(Deserialize, Debug)]
 struct FileStructure {
@@ -67,6 +68,99 @@ enum GameScene {
     Flight,
 }
 
+// Self defined type
+
+#[derive(PartialEq, Debug)]
+struct ServiceMethod {
+    name: String,    
+}
+
+#[derive(PartialEq, Debug)]
+struct ServiceProperty {
+    name: String,
+}
+
+#[derive(PartialEq, Debug)]
+struct ClassMethod {
+    class: String,
+    method: String,
+}
+
+#[derive(PartialEq, Debug)]
+struct ClassProperty {
+    class: String,
+    property: String,
+}
+
+
+#[derive(PartialEq, Debug)]
+enum ProcedureType {
+    Standard(ServiceMethod),
+    PropertyGetter(ServiceProperty),
+    PropertySetter(ServiceProperty),
+    ClassMethod(ClassMethod),
+    StaticClassMethod(ClassMethod),
+    ClassPropertyGetter(ClassProperty),
+    ClassPropertySetter(ClassProperty),
+    Unknown,
+}
+
+fn get_procedure_type(procedure_name: &str) -> ProcedureType {
+    // Patern with static/get/set in the middle
+    let re = Regex::new(r"(.*)_(get|set|static)_(.*)").unwrap();
+    let captures = re.captures(procedure_name);
+    if let Some(cap) = captures {
+        match &cap[2] {
+            "get" => {
+                return ProcedureType::ClassPropertyGetter(ClassProperty {
+                    class: (&cap[1]).to_string(),
+                    property: (&cap[3]).to_string(),
+                });
+            },
+            "set" => {
+                return ProcedureType::ClassPropertySetter(ClassProperty {
+                    class: (&cap[1]).to_string(),
+                    property: (&cap[3]).to_string(),
+                });
+            },
+            "static" => {
+                return ProcedureType::StaticClassMethod(ClassMethod {
+                    class: (&cap[1]).to_string(),
+                    method: (&cap[3]).to_string(),
+                });
+            },
+            _ => {
+                return ProcedureType::Unknown;
+            }
+        }
+    }
+    let re = Regex::new(r"(get|set|.*)_(.*)").unwrap();
+    let captures = re.captures(procedure_name);
+    if let Some(cap) = captures {
+        match &cap[1] {
+            "get" => {
+                return ProcedureType::PropertyGetter(ServiceProperty {
+                    name: (&cap[2]).to_string(),
+                })
+            },
+            "set" => {
+                return ProcedureType::PropertySetter(ServiceProperty {
+                    name: (&cap[2]).to_string(),
+                })
+            },
+            _ => {
+                return ProcedureType::ClassMethod(ClassMethod {
+                    class: (&cap[1]).to_string(),
+                    method: (&cap[2]).to_string(),
+                })
+            }
+        }
+    }
+    return ProcedureType::Standard(ServiceMethod {
+        name: procedure_name.to_string(),
+    })
+}
+
 pub fn generate_for(path: &str) {
 
     let file = File::open(path).unwrap();
@@ -75,8 +169,57 @@ pub fn generate_for(path: &str) {
     let v: FileStructure = serde_json::from_reader(reader).unwrap();
 
     for proc in &v.space_center.procedures {
-        println!("{:?}", proc.0);
+        // println!("{:?}", proc.0);
+        let procedure_type = get_procedure_type(proc.0);
+        // println!("{:?}", procedure_type);
     }
     
-    println!("{:?}", v.space_center.procedures["get_ActiveVessel"]);
+    // println!("{:?}", v.space_center.procedures["get_ActiveVessel"]);
+    
+    // Convert into some more useable structure for code generation
+    
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_standard_method() {
+        let result = get_procedure_type("WarpTo");
+        let expected = ProcedureType::Standard(ServiceMethod {
+            name: "WarpTo".to_string(),
+        });
+        assert_eq!(result, expected);
+    }
+    
+    #[test]
+    fn test_service_getter() {
+        let result = get_procedure_type("get_WarpMode");
+        let expected = ProcedureType::PropertyGetter(ServiceProperty {
+            name: "WarpMode".to_string(),
+        });
+        assert_eq!(result, expected);
+    }
+    
+    #[test]
+    fn test_class_method() {
+        let result = get_procedure_type("AutoPilot_Engage");
+        let expected = ProcedureType::ClassMethod(ClassMethod {
+            class: "AutoPilot".to_string(),
+            method: "Engage".to_string(),
+        });
+        assert_eq!(result, expected);
+    }
+    
+    #[test]
+    fn test_class_property_getter() {
+        let result = get_procedure_type("Vessel_get_Type");
+        let expected = ProcedureType::ClassPropertyGetter(ClassProperty {
+            class: "Vessel".to_string(),
+            property: "Type".to_string(),
+        });
+        assert_eq!(result, expected);
+    }
+
 }
