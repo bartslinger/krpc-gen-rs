af27db7dcef07a9c96d1d2eaee662c6934a99335
Bart Slinger <bartslinger@gmail.com>
2022-05-23 23:20:58 +0200

    do not yet generate krpc service

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,11 @@ fn main() {
     );
 
     // krpc_gen::generate_for(
+    //     std::path::Path::new("/home/bart/.local/share/Steam/steamapps/common/Kerbal Space Program/GameData/kRPC/KRPC.json"),
+    //     std::path::Path::new("/home/bart/testdir/ksp/kerbal-computer/src/generated/services/krpc.ts")
+    // );
+
+    // krpc_gen::generate_for(
     //     std::path::Path::new("/home/bart/.local/share/Steam/steamapps/common/Kerbal Space Program/GameData/kRPC/KRPC.Drawing.json"),
     //     std::path::Path::new("/home/bart/testdir/ksp/kerbal-computer/src/generated/services/drawing.ts")
     // );
