fd1b81d60951a8a93f12a96228522c26a81e7aff
Bart Slinger <bartslinger@gmail.com>
2022-05-23 23:18:51 +0200

    do not yet generate other services

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,29 @@ fn main() {
         std::path::Path::new("/home/bart/.local/share/Steam/steamapps/common/Kerbal Space Program/GameData/kRPC/KRPC.SpaceCenter.json"),
         std::path::Path::new("/home/bart/testdir/ksp/kerbal-computer/src/generated/services/space-center.ts")
     );
+
+    // krpc_gen::generate_for(
+    //     std::path::Path::new("/home/bart/.local/share/Steam/steamapps/common/Kerbal Space Program/GameData/kRPC/KRPC.Drawing.json"),
+    //     std::path::Path::new("/home/bart/testdir/ksp/kerbal-computer/src/generated/services/drawing.ts")
+    // );
+
+    // krpc_gen::generate_for(
+    //     std::path::Path::new("/home/bart/.local/share/Steam/steamapps/common/Kerbal Space Program/GameData/kRPC/KRPC.InfernalRobotics.json"),
+    //     std::path::Path::new("/home/bart/testdir/ksp/kerbal-computer/src/generated/services/infernal-robotics.ts")
+    // );
+
+    // krpc_gen::generate_for(
+    //     std::path::Path::new("/home/bart/.local/share/Steam/steamapps/common/Kerbal Space Program/GameData/kRPC/KRPC.KerbalAlarmClock.json"),
+    //     std::path::Path::new("/home/bart/testdir/ksp/kerbal-computer/src/generated/services/kerbal-alarm-clock.ts")
+    // );
+
+    // krpc_gen::generate_for(
+    //     std::path::Path::new("/home/bart/.local/share/Steam/steamapps/common/Kerbal Space Program/GameData/kRPC/KRPC.RemoteTech.json"),
+    //     std::path::Path::new("/home/bart/testdir/ksp/kerbal-computer/src/generated/services/remote-tech.ts")
+    // );
+
+    // krpc_gen::generate_for(
+    //     std::path::Path::new("/home/bart/.local/share/Steam/steamapps/common/Kerbal Space Program/GameData/kRPC/KRPC.UI.json"),
+    //     std::path::Path::new("/home/bart/testdir/ksp/kerbal-computer/src/generated/services/ui.ts")
+    // );
 }
